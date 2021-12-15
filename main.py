c70e4a31151dafd1e86778306634c0fd05491164
kristinesch <krisch@stud.ntnu.no>
2021-12-15 13:18:38 +0100

    fixed a(t)

--- main.py
+++ main.py
@@ -9,68 +9,145 @@ from matplotlib import pyplot as plt
 #Funker jo ikke for omega_K lik 0 ??
 
 """ a(t) and H(t)-----------------------------------------------------------------"""
+def g(y, t, H0, Om_M, Om_R, Om_Lambda):
+    a, aprime = y
+    dydt = [aprime, -(a/2)*H0**2*(Om_M/a**3+2*Om_R/a**4-2*Om_Lambda)]
+    return dydt
 
-def f(x,t,H0,Om_M,Om_R,Om_Lambda):
-    a=x[0]
-    return np.array([x[1],-(a/2)*H0*H0*(Om_M/(np.power(a,3))+2*Om_R/(np.power(a,4))-2*Om_Lambda)])
+# def f(x,t,H0,Om_M,Om_R,Om_Lambda):
+#     return np.array([x[1],-(x[0]/2)*H0*H0*(Om_M/(np.power(x[0],3))+2*Om_R/(np.power(x[0],4))-2*Om_Lambda)])
 
+def calculate_a_H(y0,t1,t2,H0,Om_M,Om_R,Om_Lambda):
+
+    sol1 = integrate.odeint(g, y0, t1, args=(H0, Om_M, Om_R, Om_Lambda))
+    sol2 = integrate.odeint(g, y0, t2, args=(H0, Om_M, Om_R, Om_Lambda))
+
+    # a=np.append(sol2[:,0],sol1[:,0])
+    # adot=np.append(sol2[:,1],sol1[:,1])
+    # t=np.append(t2,t1)
+    # print(t,a,adot)
+
+    # plt.plot(t1, sol1[:, 0], 'b', t2, sol2[:,0], 'r', label='a(t)')
+    # #plt.plot(t,a, 'r', label='a(t)')
+    # # plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+    # plt.legend(loc='best')
+    # plt.xlabel('t')
+    # plt.grid()
+    # plt.show()
+    return sol1,sol2
+
+def a_and_H(y0,t1,t2,H0List,Om_MList,Om_RList,Om_LambdaList):
+    sol1List=[]
+    sol2List=[]
+    colors=["red","blue","green","orange"]
+    for i in range(len(H0List)):
+        sol1,sol2=calculate_a_H(y0,t1,t2,H0List[i],Om_MList[i],Om_RList[i],Om_LambdaList[i])
+        sol1List.append(sol1)
+        sol2List.append(sol2)
+
+    figa, axa=plt.subplots(1,1)
+    figH, axH=plt.subplots(1,1)
+
+    for i in range(len(sol1List)):
+        a1=sol1List[i][:,0]
+        a2=sol2List[i][:,0]
+        adot1=sol1List[i][:,1]
+        adot2=sol2List[i][:,1]
+        H1=adot1/a1
+        H2=adot2/a2
+        #print(H1)
+        axa.plot(t1,a1, color=colors[i],label="$\Omega_M=$"+str(Om_MList[i])+" $, \Omega_R=$"+str(Om_RList[i])+
+        "$, \Omega_\Lambda=$"+str(Om_LambdaList[i])+" $, H_0$="+str(round(H0List[i],2)))
+        axa.plot(t2,a2,color=colors[i])
+
+        axH.plot(t1,H1, color=colors[i],label="$\Omega_M=$"+str(Om_MList[i])+" $, \Omega_R=$"+str(Om_RList[i])+
+        "$, \Omega_\Lambda=$"+str(Om_LambdaList[i])+" $, H_0$="+str(round(H0List[i],2)))
+        axH.plot(t2,H2,color=colors[i])
+
+    figa.suptitle("a(t)")
+    axa.set_xlabel("t [Gyr]")
+    axa.set_ylabel("a(t)")
+    figa.legend(loc="center")
+    figa.savefig("a_fig")
     
-def calculate_a_H(t,H0,Om_M,Om_R,Om_Lambda):
-    x0=np.array([1,H0])
-    x=integrate.odeint(f,x0,t, args=(H0,Om_M,Om_R,Om_Lambda))
 
-    a=x[:,0]
-    adot=x[:,1]
-    H=a/adot
-    return a, H
-
-def plot_a(t,aList,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
-    fig, ax=plt.subplots(1,1)
-    for i, ai in enumerate(aList):
-        ax.plot(t,ai, label="$\Omega_M=$,"+str(Om_Ms[i])+" $\Omega_R=$,"+str(Om_Rs[i])+"$ \Omega_\Lambda=$,"+str(Om_Lambdas[i])+" $H_0$="+str(H0s[i]))
-    ax.set_xlabel("t")
-    ax.set_ylabel("a")
-    #plt.show()
-    fig.legend()
-    fig.savefig(filename)
-    plt.show() 
-
-def plot_H(t,HList,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
-    fig, ax=plt.subplots(1,1)
-    for i, Hi in enumerate(HList):
-        ax.plot(t,Hi, label="$\Omega_M=$,"+str(Om_Ms[i])+" $\Omega_R=$,"+str(Om_Rs[i])+"$ \Omega_\Lambda=$,"+str(Om_Lambdas[i])+" $H_0$="+str(H0s[i]))
-    ax.set_xlabel("t")
-    ax.set_ylabel("H")
-    #plt.show()
-    fig.legend()
-    fig.savefig(filename)
-    plt.show() 
-
-def a_and_H(t,H0s,Om_Ms,Om_Rs,Om_Lambdas):
-    HList=[]
-    aList=[]
-    for i in range(len(H0s)):
-        a,H=calculate_a_H(t,H0s[i],Om_Ms[i],Om_Rs[i],Om_Lambdas[i])
-        aList.append(a)
-        HList.append(H)
-    plot_a(t,aList,H0s,Om_Ms,Om_Rs,Om_Lambdas,"a_fig")
-    plot_H(t,HList,H0s,Om_Ms,Om_Rs,Om_Lambdas,"H_fig")
-
-#testing stuff...
-def test():
-    t=np.linspace(0,10,1000)
-    H0=67
-    Om_M=1
-    Om_R=0
-    Om_Lambda=0
-    a,H=calculate_a_H(t,H0,Om_M,Om_R,Om_Lambda)
-    print(a)
-    plt.plot(t,H)
-    plt.show()
-    plt.plot(t,a)
+    figH.suptitle("H(t)")
+    axH.set_xlabel("t [Gyr]")
+    axH.set_ylabel("H(t)")
+    figH.legend(loc="center")
+    figH.savefig("H_fig")
     plt.show()
 
-#test()
+
+"""a and H failed attempt"""
+
+# def f(x,t,H0,Om_M,Om_R,Om_Lambda):
+#     #a=x[0]
+#     return np.array([x[1],-(x[0]/2)*H0*H0*(Om_M/(np.power(x[0],3))+2*Om_R/(np.power(x[0],4))-2*Om_Lambda)])
+
+# def g(t,x,H0,Om_M,Om_R,Om_Lambda):
+#     return np.array([x[1],-(x[0]/2)*H0*H0*(Om_M/(np.power(x[0],3)+10e-10)+2*Om_R/(np.power(x[0],4)+10e-10)-2*Om_Lambda)])
+    
+# def calculate_a_H(t,H0,Om_M,Om_R,Om_Lambda):
+#     x0=np.array([1,H0])
+#     #x=integrate.odeint(f,x0,t, args=(H0,Om_M,Om_R,Om_Lambda))
+#     sol=integrate.solve_ivp(g,[t[0],t[-1]],x0, args=(H0,Om_M,Om_R,Om_Lambda),method="LSODA")
+#     #print(sol)
+#     t=sol.t
+#     a=sol.y[0]
+#     adot=sol.y[1]
+#     print(t,a)
+#     # a=x[:,0]
+#     # adot=x[:,1]
+#     H=a/adot
+    
+#     return t,a, H
+
+# def plot_a(tList1,tList2,aList1,aList2,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
+#     fig, ax=plt.subplots(1,1)
+#     for i, ai in enumerate(aList1):
+#         ax.plot(tList1[i],ai, label="$\Omega_M=$"+str(Om_Ms[i])+" $, \Omega_R=$"+str(Om_Rs[i])+"$, \Omega_\Lambda=$"+str(Om_Lambdas[i])+" $, H_0$="+str(H0s[i]))
+#         ax.plot(tList2[i],aList2[i])
+#     ax.set_xlabel("t")
+#     ax.set_ylabel("a")
+#     fig.legend()
+#     fig.savefig(filename)
+#     plt.show() 
+
+# def plot_H(tList1,tList2,HList1,HList2,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
+#     fig, ax=plt.subplots(1,1)
+#     for i, Hi in enumerate(HList1):
+#         ax.plot(tList1[i],Hi, label="$\Omega_M=$"+str(Om_Ms[i])+" $, \Omega_R=$"+str(Om_Rs[i])+"$, \Omega_\Lambda=$"+str(Om_Lambdas[i])+" $, H_0$="+str(H0s[i]))
+#         ax.plot(tList2,HList2[i])
+#     ax.set_xlabel("t")
+#     ax.set_ylabel("H")
+#     fig.legend()
+#     fig.savefig(filename)
+#     plt.show() 
+
+# def a_and_H(t1,t2,H0s,Om_Ms,Om_Rs,Om_Lambdas):
+#     HList1=[]
+#     aList1=[]
+#     tList1=[]
+
+#     HList2=[]
+#     aList2=[]
+#     tList2=[]
+#     for i in range(len(H0s)):
+#         t,a,H=calculate_a_H(t1,H0s[i],Om_Ms[i],Om_Rs[i],Om_Lambdas[i])
+#         aList1.append(a)
+#         HList1.append(H)
+#         tList1.append(t)
+
+#         t,a,H=calculate_a_H(t2,H0s[i],Om_Ms[i],Om_Rs[i],Om_Lambdas[i])
+#         aList2.append(a)
+#         HList2.append(H)
+#         tList2.append(t)
+
+
+#     plot_a(tList1,tList2,aList1,aList2,H0s,Om_Ms,Om_Rs,Om_Lambdas,"a_fig")
+#     plot_H(tList1,tList2,HList1,HList2,H0s,Om_Ms,Om_Rs,Om_Lambdas,"H_fig")
+
 
 
 
@@ -110,10 +187,9 @@ def plot_distance(z,ds,filename,dstring,Om_M,Om_R,Om_Lambda,H0): #ds a list
     #colors=[""]
     fig,ax=plt.subplots(1,1)
     for i, di in enumerate(ds):
-        ax.plot(z,di,label="$\Omega_M=$,"+str(Om_M[i])+" $\Omega_R=$,"+str(Om_R[i])+"$ \Omega_\Lambda=$,"+str(Om_Lambda[i])+" $H_0$="+str(H0[i]))
+        ax.plot(z,di,label="$\Omega_M=$"+str(Om_M[i])+" $,\Omega_R=$"+str(Om_R[i])+"$, \Omega_\Lambda=$"+str(Om_Lambda[i])+" $, H_0$="+str(H0[i]))
     ax.set_xlabel("z")
     ax.set_ylabel(dstring)
-    #plt.show()
     fig.legend()
     fig.savefig(filename)
     plt.show()
@@ -151,28 +227,38 @@ def distances(z,Om_R, Om_M, Om_Lambda, H0,k):
 def main():
     year=365*24*60*60
     #Take as input?
-    H0=1000/3.086e22*(3600*24*365*10e09)*67
-    
-    H0s=[H0,H0,H0,H0,H0]
-    Om_Ms=[0,0.3,0.5,0,0.4]
-    Om_Rs=[0,0,0,0.5,0]
-    Om_Lambdas=[0,0,0,0,0.5]
+    H0=1000/3.086e22*(3600*24*365*1e09)*67
+    #H0=67
+    H0s=[H0,H0,H0,H0]
+    Om_Ms=[0.3,0.3,5,1]
+    Om_Rs=[0,0,0,0]
+    Om_Lambdas=[0,0.7,0,0]
 
     k=1
 
-    Nz=10
-    z_min=0
+    Nz=1000
+    z_min=-15
     z_max=15
     z=np.linspace(z_min,z_max,Nz)
 
-    Nt=10000
+    Nt=10
     
-    t_min=-10
-    t_max=20
-    t=np.linspace(t_min,t_max,Nt)
+    t_min=-14.15
+    t_max=30
+    t1 = np.linspace(0, t_max, Nt)
+    t2 = np.linspace(0, t_min, Nt)
+
+    y0=np.array([1,H0])
 
     #distances(z,Om_Rs, Om_Ms, Om_Lambdas, H0s,k)
-    a_and_H(t, H0s, Om_Ms,Om_Rs,Om_Lambdas)
+
+    Om_M = 0.3
+    Om_R = 0
+    Om_Lambda = 0.7
+
+    #calculate_a_H(y0,t1,t2,H0,Om_M,Om_R,Om_Lambda)
+
+    a_and_H(y0,t1, t2, H0s, Om_Ms,Om_Rs,Om_Lambdas)
 
 
 
@@ -192,3 +278,6 @@ main()
 # plt.plot(t,sol[:,0])
 # plt.show()
 
+a=np.array([1,2])
+b=np.array([3,4])
+print(np.append(a,b))
---
+++ test.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import numpy as np
+
+def pend(y, t, H0, Om_M, Om_R, Om_Lambda):
+    a, aprime = y
+    dydt = [aprime, -(a/2)*H0**2*(Om_M/a**3+2*Om_R/a**4-2*Om_Lambda)]
+    return dydt
+
+# We assume the constants are:
+H0=1000/3.086e22*(3600*24*365*1e09)*67
+Om_M = 0.3
+Om_R = 0
+Om_Lambda = 0.7
+# Initial conditions
+y0 = [1, H0]
+
+t = np.linspace(0, 30, 1000)
+t2 = np.linspace(0, -14.15, 1000)
+
+sol = odeint(pend, y0, t, args=(H0, Om_M, Om_R, Om_Lambda))
+sol2 = odeint(pend, y0, t2, args=(H0, Om_M, Om_R, Om_Lambda))
+
+plt.plot(t, sol[:, 0], 'b', t2, sol2[:,0], 'r', label='a(t)')
+# plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
