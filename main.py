86b74183b9a7555f2aad5c1383fb50cb7fe57cd8
kristinesch <krisch@stud.ntnu.no>
2021-12-15 01:17:20 +0100

    tried to solve a(t) NB: not working

--- main.py
+++ main.py
@@ -8,6 +8,81 @@ from matplotlib import pyplot as plt
 #TODO fiks dL for neg rot
 #Funker jo ikke for omega_K lik 0 ??
 
+""" a(t) and H(t)-----------------------------------------------------------------"""
+
+def f(x,t,H0,Om_M,Om_R,Om_Lambda):
+    a=x[0]
+    #return np.array([x[1],-(a/2)*H0*H0*(Om_M/(np.power(a,3))+2*Om_R/(np.power(a,4))-2*Om_Lambda)])
+    return np.array([x[1],-(1/2)*H0*H0*(Om_M/(np.power(a,1))+2*Om_R/(np.power(a,2))-2*Om_Lambda)])
+#testing....
+def g(x,t,b,c):
+    a=x[0]
+    return np.array([x[1],-a*c*(b/np.power(a,3)+c)])
+    
+def calculate_a_H(t,H0,Om_M,Om_R,Om_Lambda):
+    x0=np.array([H0,1])
+    x=integrate.odeint(f,x0,t, args=(H0,Om_M,Om_R,Om_Lambda))
+    #b=2
+    #c=2
+    #x=integrate.odeint(g,x0,t,args=(b,c))
+    #print(x)
+    a=x[:,0]
+    adot=x[:,1]
+    H=a/adot
+    return a, H
+
+def plot_a(t,aList,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
+    fig, ax=plt.subplots(1,1)
+    for i, ai in enumerate(aList):
+        ax.plot(t,ai, label="$\Omega_M=$,"+str(Om_Ms[i])+" $\Omega_R=$,"+str(Om_Rs[i])+"$ \Omega_\Lambda=$,"+str(Om_Lambdas[i])+" $H_0$="+str(H0s[i]))
+    ax.set_xlabel("t")
+    ax.set_ylabel("a")
+    #plt.show()
+    fig.legend()
+    fig.savefig(filename)
+    plt.show() 
+
+def plot_H(t,HList,H0s,Om_Ms,Om_Rs,Om_Lambdas,filename):
+    fig, ax=plt.subplots(1,1)
+    for i, Hi in enumerate(HList):
+        ax.plot(t,Hi, label="$\Omega_M=$,"+str(Om_Ms[i])+" $\Omega_R=$,"+str(Om_Rs[i])+"$ \Omega_\Lambda=$,"+str(Om_Lambdas[i])+" $H_0$="+str(H0s[i]))
+    ax.set_xlabel("t")
+    ax.set_ylabel("H")
+    #plt.show()
+    fig.legend()
+    fig.savefig(filename)
+    plt.show() 
+
+def a_and_H(t,H0s,Om_Ms,Om_Rs,Om_Lambdas):
+    HList=[]
+    aList=[]
+    for i in range(len(H0s)):
+        a,H=calculate_a_H(t,H0s[i],Om_Ms[i],Om_Rs[i],Om_Lambdas[i])
+        aList.append(a)
+        HList.append(H)
+    plot_a(t,aList,H0s,Om_Ms,Om_Rs,Om_Lambdas,"a_fig")
+    plot_H(t,HList,H0s,Om_Ms,Om_Rs,Om_Lambdas,"H_fig")
+
+
+def test():
+    t=np.linspace(0,10,1000)
+    H0=67
+    Om_M=1
+    Om_R=0
+    Om_Lambda=0
+    a,H=calculate_a_H(t,H0,Om_M,Om_R,Om_Lambda)
+    print(a)
+    plt.plot(t,H)
+    plt.show()
+    plt.plot(t,a)
+    plt.show()
+
+#test()
+
+
+
+"""dL and dA------------------------------------------------------------------"""
+
 def E(z,Om_R, Om_M, Om_Lambda, Om_K): #z is a np array
     pM=np.power((1+z),3)
     pR=np.power((1+z),4)
@@ -50,7 +125,7 @@ def plot_distance(z,ds,filename,dstring,Om_M,Om_R,Om_Lambda,H0): #ds a list
     fig.savefig(filename)
     plt.show()
 
-def distances(z,Om_R, Om_M, Om_Lambda, H0,k):
+def calculate_dA_dL(z,Om_R, Om_M, Om_Lambda, H0,k):
     #Om_K=(-const.c*const.c*k)/(H0*H0)
     Om_K=1-Om_Lambda-Om_M-Om_R
     print("K:",Om_K)
@@ -62,27 +137,13 @@ def distances(z,Om_R, Om_M, Om_Lambda, H0,k):
     #plot_distance(z,dA,"dAfig","dA")
     return dL,dA
 
-def main():
 
-    #Take as input?
-    H0=[67,67,67,67,67]
-    Om_M=[0,0.3,0.5,0,0]
-    Om_R=[0,0,0,0.5,0]
-    Om_Lambda=[0,0,0,0,0.5]
-
-    k=1
-
-    N=10
-    z_min=0
-    z_max=15
-    z=np.linspace(z_min,z_max,N)
-
+def distances(z,Om_R, Om_M, Om_Lambda, H0,k):
     #running distances etc
     dLs=[]
     dAs=[]   
     for i in range(len(H0)):
-        print(i)
-        dLi,dAi= distances(z,Om_R[i], Om_M[i], Om_Lambda[i], H0[i],k)
+        dLi,dAi= calculate_dA_dL(z,Om_R[i], Om_M[i], Om_Lambda[i], H0[i],k)
         dLs.append(dLi)
         dAs.append(dAi)
 
@@ -90,6 +151,48 @@ def main():
     plot_distance(z,dLs,"dLfig","dL",Om_M,Om_R,Om_Lambda,H0)
 
 
+"""--------------------------------------------------------------------"""
+
+
+
+def main():
+
+    #Take as input?
+    H0s=[67,67,67,67,67]
+    Om_Ms=[0,0.3,0.5,0,0.4]
+    Om_Rs=[0,0,0,0.5,0]
+    Om_Lambdas=[0,0,0,0,0.5]
+
+    k=1
+
+    Nz=10
+    z_min=0
+    z_max=15
+    z=np.linspace(z_min,z_max,Nz)
+
+    Nt=10000
+    year=365*24*60*60
+    t_min=0
+    t_max=1000
+    t=np.linspace(t_min,t_max,Nt)
+
+    #distances(z,Om_Rs, Om_Ms, Om_Lambdas, H0s,k)
+    a_and_H(t, H0s, Om_Ms,Om_Rs,Om_Lambdas)
+
+
+
+
 
 main()
 
+
+
+# def k(x,t,a,b):
+#     return np.array([x[1],a+b])
+
+# t=np.linspace(0,10,100)
+# a=2
+# b=0
+# sol=integrate.odeint(k,[0,0],t,args=(a,b))
+# plt.plot(t,sol[:,0])
+# plt.show()
